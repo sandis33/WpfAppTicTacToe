31213748c1b093016b8ba4786dae0d41daf90dba
Datorium <hello@datorium.eu>
2022-01-06 19:39:09 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppTicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string turn = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            turn = "X";
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock tb = (TextBlock)sender;
+            
+            if(tb.Text != "X" && tb.Text != "O")
+            {
+                tb.Text = turn;
+                SwitchTurn();
+            }       
+        }
+
+        private void SwitchTurn()
+        {
+            if (turn == "X")
+            {
+                turn = "O";
+            }
+            else
+            {
+                turn = "X";
+            }
+        }
+    }
+}
