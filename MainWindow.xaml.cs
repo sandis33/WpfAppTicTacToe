f1a4c7877c194b3b7d107ccfde6af070d92f8f62
sandis33 <sandishusca@inbox.lv>
2022-01-13 19:25:32 +0200

    Added detection for win

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -40,6 +40,7 @@ namespace WpfAppTicTacToe
             if(tb.Text != "X" && tb.Text != "O")
             {
                 tb.Text = turn;
+                CheckForWin();
                 SwitchTurn();
             }       
         }
@@ -54,6 +55,92 @@ namespace WpfAppTicTacToe
             {
                 turn = "X";
             }
+        }
+
+        //private void CheckForWin()
+        //{
+        //    if (tb1.Text != "   " && tb1.Text == tb2.Text && tb2.Text == tb3.Text)
+        //    {
+        //        GameOver();
+        //    }
+        //    else if(tb4.Text != "   " && tb4.Text == tb5.Text && tb5.Text == tb6.Text)
+        //    {
+        //        GameOver();
+        //    }
+        //    else if(tb1.Text != "   " && tb7.Text == tb8.Text && tb8.Text == tb9.Text)
+        //    {
+        //        GameOver();
+        //    }
+        //    else if(tb1.Text != "   " && tb1.Text == tb4.Text && tb4.Text == tb7.Text)
+        //    {
+        //        GameOver();
+        //    }
+        //    else if(tb1.Text != "   " && tb2.Text == tb5.Text && tb5.Text == tb8.Text)
+        //    {
+        //        GameOver();
+        //    }
+        //    else if(tb1.Text != "   " && tb3.Text == tb6.Text && tb6.Text == tb9.Text)
+        //    {
+        //        GameOver();
+        //    }
+        //    else if(tb1.Text != "   " && tb1.Text == tb5.Text && tb5.Text == tb9.Text)
+        //    {
+        //        GameOver();
+        //    }
+        //    else if(tb1.Text != "   " && tb3.Text == tb5.Text && tb5.Text == tb7.Text)
+        //    {
+        //        GameOver();
+        //    }
+        //}
+        private void CheckForWin()
+        {
+            var winColor = Brushes.Orange;
+
+            if (tb1.Text != "   " && tb1.Text == tb2.Text && tb2.Text == tb3.Text)
+            {
+                tb1.Foreground = tb2.Foreground = tb3.Foreground = winColor;
+                GameOver();
+            }
+            else if (tb4.Text != "   " && tb4.Text == tb5.Text && tb5.Text == tb6.Text)
+            {
+                tb4.Foreground = tb5.Foreground = tb6.Foreground = winColor;
+                GameOver();
+            }
+            else if (tb7.Text != "   " && tb7.Text == tb8.Text && tb8.Text == tb9.Text)
+            {
+                tb7.Foreground = tb8.Foreground = tb9.Foreground = winColor;
+                GameOver();
+            }
+            else if (tb1.Text != "   " && tb1.Text == tb4.Text && tb4.Text == tb7.Text)
+            {
+                tb1.Foreground = tb4.Foreground = tb7.Foreground = winColor;
+                GameOver();
+            }
+            else if (tb2.Text != "   " && tb2.Text == tb5.Text && tb5.Text == tb8.Text)
+            {
+                tb2.Foreground = tb5.Foreground = tb8.Foreground = winColor;
+                GameOver();
+            }
+            else if (tb3.Text != "   " && tb3.Text == tb6.Text && tb6.Text == tb9.Text)
+            {
+                tb3.Foreground = tb6.Foreground = tb9.Foreground = winColor;
+                GameOver();
+            }
+            else if (tb1.Text != "   " && tb1.Text == tb5.Text && tb5.Text == tb9.Text)
+            {
+                tb1.Foreground = tb5.Foreground = tb9.Foreground = winColor;
+                GameOver();
+            }
+            else if (tb3.Text != "   " && tb3.Text == tb5.Text && tb5.Text == tb7.Text)
+            {
+                tb3.Foreground = tb5.Foreground = tb7.Foreground = winColor;
+                GameOver();
+            }
+        }
+
+        private void GameOver()
+        {
+            MessageBox.Show($"Game Over, {turn} Wins!");
         }
     }
 }
